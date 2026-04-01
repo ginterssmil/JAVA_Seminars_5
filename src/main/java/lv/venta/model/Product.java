859b247e1e4791a77b15c9e0c886e4fe75fdeea2
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-04-01 08:25:23 +0300

    new: class Product and enum ProductType

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,13 @@
 package lv.venta.model;
 
 public class Product {
-
+	private long id;
+	private String title;
+	private float price;
+	private int quantity;
+	private String description;
+	private ProductType productType;
+	
+	
+	
 }
