47fc0963f1a1ad22325161020e6a74b71dbc0428
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-04-01 08:50:30 +0300

    PRoduct setters gettters tostring constructors

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,12 @@
 package lv.venta.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
 	private long id;
 	private String title;
@@ -8,6 +15,14 @@ public class Product {
 	private String description;
 	private ProductType productType;
 	
+	//getters - nak no lombok bibliotekas
+	//setters - nak no lombok bibliotekas
 	
-	
+	public Product(String inputTitle, float inputPrice, int inputQuantity, String inputDescription,ProductType inputProductType) {
+		setTitle(inputTitle);
+		setPrice(inputPrice);
+		setQuantity(inputQuantity);
+		setDescription(inputDescription);
+		setProductType(inputProductType);
+	}
 }
