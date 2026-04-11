05ccb7e46e78cd87af3bc2cac850c7089b5241b1
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-04-11 08:22:31 +0300

    Edit: commandlinerunner function to test repo

--- src/main/java/lv/venta/JavaSeminars05Application.java
+++ src/main/java/lv/venta/JavaSeminars05Application.java
@@ -1,7 +1,13 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Product;
+import lv.venta.model.ProductType;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class JavaSeminars05Application {
@@ -10,4 +16,25 @@ public class JavaSeminars05Application {
 		SpringApplication.run(JavaSeminars05Application.class, args);
 	}
 
+@Bean	
+	public CommandLineRunner testRepo(IProductRepo prodRepo) {
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				
+				Product prod1 = new Product("Abols", (float)0.99, 5, "Polija", ProductType.fruit);
+				Product prod2 = new Product("Burkans", (float)0.39, 5, "oranzs", ProductType.vegetable);
+				Product prod3 = new Product("Arbuzs", (float)4.59, 20, "bez seklam", ProductType.fruit);
+				
+				prodRepo.save(prod1);
+				prodRepo.save(prod2);
+				prodRepo.save(prod3);
+				
+				
+			}
+		};
+		
+	}
 }
--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -1,5 +1,8 @@
 package lv.venta.repo;
 
-public interface IProductRepo {
+import org.springframework.data.repository.CrudRepository;
+import lv.venta.model.Product;
 
+public interface IProductRepo extends CrudRepository<Product, Long>{
+	//TODO Papildinat pec nepieicsamibas citas funkcijas
 }
